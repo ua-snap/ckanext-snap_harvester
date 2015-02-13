6dd643266b6d5514db066840eef8eae5de37d84b
Bruce Crevensten <bruce.crevensten@gmail.com>
2015-02-13 18:24:04 +0000

    adds maintainer name + email and version number

--- ckanext/snap_harvester/plugin.py
+++ ckanext/snap_harvester/plugin.py
@@ -51,19 +51,27 @@ class SnapHarvester(CSWHarvester, SingletonPlugin):
         for extra in package_dict['extras']:
             extras[extra['key']] = extra['value']
 
-        # Will be a list of names.
+        # Version of data set: called Edition in GN
+        package_dict['version'] = tree.xpath('//gmd:edition/gco:CharacterString/text()', namespaces=namespaces)
+
+        # Maintainer name/email
+        package_dict['maintainer'] = tree.xpath('//gmd:pointOfContact/gmd:CI_ResponsibleParty/gmd:individualName/gco:CharacterString/text()', namespaces=namespaces)
+        package_dict['maintainer_email'] = tree.xpath('//gmd:pointOfContact/gmd:CI_ResponsibleParty/gmd:contactInfo/gmd:CI_Contact/gmd:address/gmd:CI_Address/gmd:electronicMailAddress/gco:CharacterString/text()', namespaces=namespaces)
+
+        # Credits: Will be a list of names.
         credits = tree.xpath('//gmd:credit/gco:CharacterString/text()', namespaces=namespaces)
         extras['credits'] = json.dumps(credits)
 
-        # Will get two values, one for x and one for y; we can assume square pixels for the moment.
+        # Spatial resolution: Will get two values, one for x and one for y; we can assume square pixels for the moment.
         spatial_resolution = tree.xpath('//gmd:spatialRepresentationInfo/gmd:MD_Georectified/gmd:axisDimensionProperties/gmd:MD_Dimension/gmd:resolution/gco:Angle/text()', namespaces=namespaces)[0]
         spatial_resolution_units = tree.xpath('//gmd:spatialRepresentationInfo/gmd:MD_Georectified/gmd:axisDimensionProperties/gmd:MD_Dimension/gmd:resolution/gco:Angle/@uom', namespaces=namespaces)[0]
         extras['spatial-resolution'] = spatial_resolution
         extras['spatial-resolution-units'] = spatial_resolution_units
 
-        # The way we fetch temporal-start and temporal-end can be different from what the built-in xpath uses.
+        # Temporal extent: The way we fetch temporal-start and temporal-end can be different from what the built-in xpath uses.
         temporal_extent_begin = tree.xpath('//gml:TimePeriod/gml:begin//gml:timePosition/text()', namespaces=namespaces)
         temporal_extent_end = tree.xpath('//gml:TimePeriod/gml:end//gml:timePosition/text()', namespaces=namespaces)
+        log.debug('Got temporal extents, begin {0} end {1}'.format(temporal_extent_begin, temporal_extent_end))
         if(temporal_extent_begin and temporal_extent_end):
             extras['temporal-extent-begin'] = temporal_extent_begin[0]
             extras['temporal-extent-end'] = temporal_extent_end = temporal_extent_end[0]
@@ -79,5 +87,6 @@ class SnapHarvester(CSWHarvester, SingletonPlugin):
         package_dict['license_url'] = 'http://www.opendefinition.org/licenses/cc-by'
         package_dict['extras'].append({'key':'license', 'value':'Attribution-NonCommercial-ShareAlike 3.0 Unported'})
         package_dict['extras'].append({'key':'license_url', 'value':'http://creativecommons.org/licenses/by-nc-sa/3.0/'})
+        pprint(package_dict)
 
         return package_dict
